ee227f515de7aa94f28ea3b764df87bb2128dd36
DwarakanathAkkala <dwarakanath.akkala@gmail.com>
2024-02-09 11:38:46 +0530

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -17,24 +17,25 @@ function changeTabFocus(e) {
 
     if (e.keyCode == keydownLeft || e.keyCode == keydownRight) {
         tabs[tabFocus].setAttribute("tabindex", -1);
-    }
 
-    if (e.keyCode === keydownRight) {
-        tabFocus++;
-        if (tabFocus >= tabs.length) {
-            tabFocus = 0;
+        if (e.keyCode === keydownRight) {
+            tabFocus++;
+            if (tabFocus >= tabs.length) {
+                tabFocus = 0;
+            }
         }
-    }
 
-    if (e.keyCode === keydownLeft) {
-        tabFocus--;
-        if (tabFocus < 0) {
-            tabFocus = tabs.length - 1;
+        else if (e.keyCode === keydownLeft) {
+            tabFocus--;
+            if (tabFocus < 0) {
+                tabFocus = tabs.length - 1;
+            }
         }
+
+        tabs[tabFocus].setAttribute("tabindex", 0);
+        tabs[tabFocus].focus();
     }
 
-    tabs[tabFocus].setAttribute("tabindex", 0);
-    tabs[tabFocus].focus();
 }
 
 function changeTabPanel(e) {
@@ -44,6 +45,12 @@ function changeTabPanel(e) {
 
     const tabContainer = targetTab.parentNode;
     const mainContainer = tabContainer.parentNode;
+
+    tabContainer
+        .querySelector('[aria-selected="true"]')
+        .setAttribute("aria-selected", false);
+
+    targetTab.setAttribute("aria-selected", true);
 
     mainContainer
         .querySelectorAll('[role="tabpanel"]')
